b8d81d623c374c4983dafc48b6a3fcacf3babd52
maneesha pr <76961525+MANEESHAPR535@users.noreply.github.com>
2021-06-07 11:08:46 +0530

    Update studentdet.java

--- studentdet.java
+++ studentdet.java
@@ -22,15 +22,15 @@ class result extends sports implements student
 {
     void display()
     {
-		System.out.println("************************");
+	System.out.println("************************");
         System.out.println("Details of students are:");
         System.out.println("Name:"+name);
         System.out.println("Roll number:"+roll);
         System.out.println("Class Name:"+classname);
-		System.out.println("os:"+OS);
-		System.out.println("OOP:"+OOPs);
-		System.out.println("BM:"+BM);
-		System.out.println("total Mark of sub:"+total);
+	System.out.println("os:"+OS);
+	System.out.println("OOP:"+OOPs);
+	System.out.println("BM:"+BM);
+	System.out.println("total Mark of sub:"+total);
         System.out.println("Sports Mark:"+marks);
 		 float gt=(float)total+(float)marks;
 		 System.out.println("Accademic marks:"+gt);
@@ -47,4 +47,33 @@ public class studentdet
         res.display();
     }
 }
+
+
+
+/***************************OUTPUT***********************/
+
+
+C:\Javaprg>javac Studentdet.java
+
+C:\Javaprg>java studentdet
+Enter marks out of 20:
+17
+************************
+Details of students are:
+Name:MANEESHA PR
+Roll number:30
+Class Name:MCA
+os:85
+OOP:96
+BM:95
+total Mark of sub:276
+Sports Mark:17
+Accademic marks:293.0
+percentage:97.66667
+
+C:\Javaprg>
+
+
+
+
 
