fa1f89e0e31f3415261d58296b910db751bbd002
maneesha pr <76961525+MANEESHAPR535@users.noreply.github.com>
2021-06-15 13:11:45 +0530

    Add files via upload

---
+++ Strmanipulation.java
@@ -0,0 +1,90 @@
+
+import java.awt.*;
+import java.util.Scanner;
+class manipulation
+ {
+    Scanner sc = new Scanner(System.in);
+    private static String str;
+    void get()
+    {
+        System.out.println("Enter a string:");
+        str=sc.next();
+    }
+    void comapare()
+    {
+        System.out.println("Enter another stirng to comapare:");
+        String str2=sc.next();
+        if(str.compareTo(str2)==0)
+        {
+            System.out.println("strings are  same");
+        }
+        else
+        {
+            System.out.println("strings are not same");
+        }
+    }
+    void conacat()
+    {
+        System.out.println("Enter another stirng to comapare:");
+        String str2=sc.next();
+        String str3=str+str2;
+        System.out.println(str3);
+    }
+    void length()
+    {
+        System.out.println("Length of "+str+" is:"+str.length());
+    }
+    void upper_lower()
+    {
+        System.out.println("Upper case:"+str.toUpperCase());
+        System.out.println("Lower case:"+str.toLowerCase());
+    }
+    void reverse()
+    {
+        StringBuilder sb=new StringBuilder(str);
+        System.out.println("Reverse of "+str+" is:"+sb.reverse());
+    }
+    
+}
+public class Strmanipulation
+ {
+    public static void main(String args[]) 
+	{
+        int n;
+        manipulation m = new manipulation();
+        Scanner sc = new Scanner(System.in);
+        m.get();
+        do 
+		{
+            System.out.println(" \n MENU \n1.Length\n2.Comapare\n3.Concatinate\n4.Upper case & Lower case\n5.Reverse\n6.Exit ");
+            n = sc.nextInt();
+            switch (n)
+			{
+                case 1:
+                    m.length();
+                    break;
+                case 2:
+                    m.comapare();
+                    break;
+                case 3:
+                    m.conacat();
+                    break;
+                case 4:
+                    m.upper_lower();
+                    break;
+                case 5:
+                    m.reverse();
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+
+        }
+		while (n != 6) ;
+    }
+
+}
